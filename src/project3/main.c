81fe5509741b55b2cb4f60c91df6ddf27a17718e
Emanuel <alvarez-emanuel@outlook.com>
2024-09-28 21:31:52 -0400

    initial commit

---
+++ src/project3/main.c
@@ -0,0 +1,8 @@
+// ************************************
+// Project 3: Control LED using PWM
+// 
+// Author: Emanuel Alvarez
+// Date: 09/28/2024
+// Purpose: This project controls the brightness of an LED connected to pin PB1
+//              using PWM.
+// ************************************
